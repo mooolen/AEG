d5b489f7698060d4738fe8e6d3e9eb6f12b0dd1e
mooolen <molenfernando@gmail.com>
2013-08-23 16:12:40 +0800

    initial commit

---
+++ app_classes/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from app_classes.models import Section, Subject, ClassList
+
+admin.site.register(Section)
+admin.site.register(Subject)
+admin.site.register(ClassList)
