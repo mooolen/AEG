3a240687f557eb012b9021c457c406ae5a84e720
emsia <esia.rizal@gmail.com>
2013-08-24 21:12:36 +0800

    Ef commit

--- AEG/settings.py
+++ AEG/settings.py
@@ -1,4 +1,6 @@
 # Django settings for AEG project.
+import os.path
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -61,7 +63,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -72,7 +74,7 @@ STATICFILES_DIRS = (
 	# Put strings here, like "/home/html/static" or "C:/www/django/static".
 	# Always use forward slashes, even on Windows.
 	# Don't forget to use absolute paths, not relative paths.
-	'/home/molen/Desktop/AEG/static',
+	'C:/Documents and Settings/Admin/Desktop/AEG/static',
 )
 
 # List of finder classes that know how to find static files in
@@ -112,7 +114,7 @@ TEMPLATE_DIRS = (
 	# Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
 	# Always use forward slashes, even on Windows.
 	# Don't forget to use absolute paths, not relative paths.
-	'/home/molen/Desktop/AEG/templates',
+	'C:/Documents and Settings/Admin/Desktop/AEG/templates',
 )
 
 INSTALLED_APPS = (
--- app_auth/urls.py
+++ app_auth/urls.py
@@ -5,6 +5,6 @@ from app_auth import views
 urlpatterns = patterns('',
 	#url(r'^$', views.user_login, name='user_login'),
 	url(r'^$', views.LoginView.as_view(), name='user_login'),
-	url(r'^logout$', views.user_logout, name='user_logout')
-	
+	url(r'^logout$', views.user_logout, name='user_logout'),
+	url(r'^login$', views.LoginView.as_view(), name='login'),
 )
