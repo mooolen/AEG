f847f552597ea7ec761bf365391a22fecb7f5fb6
Molen Fernando <molenfernando@gmail.com>
2013-08-28 14:30:57 +0800

    added media url

--- AEG/urls.py
+++ AEG/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+from AEG import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
 	#url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^', include('app_auth.urls', namespace='auth')),
 	url(r'^', include('app_classes.urls')),
