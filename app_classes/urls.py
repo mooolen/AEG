c2fe55d862d95761bad7ed0f1fe7d1b24c2f7d34
mooolen <molenfernando@gmail.com>
2013-08-28 14:35:13 +0800

    fix codes

--- app_classes/urls.py
+++ app_classes/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import patterns, url
 from app_classes import views
 
 urlpatterns = patterns('',
-	url(r'^dashboard/$', views.dashboard),	
+	url(r'^dashboard/$', views.dashboard),
+	url(r'^class/$', views.class_list),
+	url(r'^class/add$', views.add_class),
 )
