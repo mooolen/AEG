bd1eb4206193476545cc01e6471eb80f7d5146d1
Molen Fernando <molenfernando@gmail.com>
2013-08-28 14:35:13 +0800

    fix codes

--- app_classes/views.py
+++ app_classes/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+from app_auth.models import UserProfile
 
 def dashboard(request):
-	return render(request, 'app_classes/dashboard2.html')
+	avatar = UserProfile.objects.get(user_id = request.user.id).avatar
+	return render(request, 'app_classes/dashboard.html', {'avatar':avatar})
+	
+def add_class(request):
+	avatar = UserProfile.objects.get(user_id = request.user.id).avatar
+	return render(request, 'app_classes/add_class.html', {'avatar':avatar})
+	
+def class_list(request):
+	return 'hello'
