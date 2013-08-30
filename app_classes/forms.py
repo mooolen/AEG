5c5923544ad8d36520c8312f99eb489ef95b733f
fsvaeg <fsvaeg@gmail.com>
2013-08-30 11:42:29 +0800

    Fixing bugs

--- app_classes/forms.py
+++ app_classes/forms.py
@@ -7,7 +7,6 @@ class addClassForm(forms.Form):
 	school = forms.ModelChoiceField(label='School', queryset=School.objects.all(), widget=forms.Select(attrs={'class':'select select-block span12', }))
 	year_level = forms.CharField(label='Level', widget=forms.TextInput(attrs={'type':'text', 'class': 'span4', 'placeholder': 'Year Level', 'min': 1, }))
 	section_name = forms.CharField(label='Section Name', widget=forms.TextInput(attrs={'type':'text', 'class': 'span4', 'placeholder': 'Class Name',}))
-	emails = forms.CharField(label='Emails', widget=forms.Textarea(attrs={'type':'text', 'class': 'input-xlarge span4', 'placeholder': 'Each emails are separeted by comma. Example: cheryleighverano@gmail.com, emsia@upd.edu.ph, molen.fenando@gmail.com',}))
-
+	
 	class Meta:
 		model = Section
