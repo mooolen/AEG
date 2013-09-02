d1d08bb52b84d610793c37549e2aedf6885a7b7e
mooolen <molenfernando@gmail.com>
2013-09-02 16:18:24 +0800

    fix codes

--- app_classes/forms.py
+++ app_classes/forms.py
@@ -7,6 +7,6 @@ class addClassForm(forms.Form):
 	school = forms.ModelChoiceField(label='School', queryset=School.objects.all(), widget=forms.Select(attrs={'class':'select select-block span12', }))
 	year_level = forms.CharField(label='Level', widget=forms.TextInput(attrs={'type':'text', 'class': 'span4', 'placeholder': 'Year Level', 'min': 1, }))
 	section_name = forms.CharField(label='Section Name', widget=forms.TextInput(attrs={'type':'text', 'class': 'span4', 'placeholder': 'Class Name',}))
-	
+
 	class Meta:
 		model = Section
