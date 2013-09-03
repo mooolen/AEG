d30d29596c92551001e390c19b1e818dcfc9f0a0
fsvaeg <fsvaeg@gmail.com>
2013-09-03 16:28:42 +0800

    me

--- app_classes/views.py
+++ app_classes/views.py
@@ -8,8 +8,6 @@ from app_auth.models import UserProfile
 from django.db.models import Count, F
 from django.contrib.auth.decorators import login_required
 
-from app_classes.forms import addClassForm
-
 @login_required(redirect_field_name='', login_url='/')
 def dashboard(request):
 	avatar = UserProfile.objects.get(user_id = request.user.id).avatar
@@ -32,14 +30,14 @@ def class_teacher(request):
 @login_required(redirect_field_name='', login_url='/')
 def teacher_addNewClass(request, add_form=None):
 	avatar = UserProfile.objects.get(user_id = request.user.id).avatar
-	addClass_form = add_form or addClassForm()
+	addClass_form = add_form or SectionForm()
 	return render(request, 'app_classes/teacher_addNewClass.html', 
 			{'addClassForm' : addClass_form, 'next_url': '/classes', 'avatar':avatar, 'active_nav':'CLASSES'})
 
 @login_required(redirect_field_name='', login_url='/')
 def submit(request):
 	if request.method == "POST":
-		form_class = addClassForm(data=request.POST)
+		form_class = SectionForm(data=request.POST)
 		success_url = request.POST.get("next_url", "/")
 		if form_class.is_valid():
 			form_class = SectionForm(data=request.POST)
@@ -47,17 +45,14 @@ def submit(request):
 			return redirect(success_url)
 		else:
 			return teacher_addNewClass(request, form_class)
-<<<<<<< HEAD
 
 @login_required(redirect_field_name='', login_url='/')
 def edit(request):
 	avatar = UserProfile.objects.get(user_id = request.user.id).avatar
 	return render(request, 'app_classes/teacher_editClass.html', {'avatar':avatar, 'active_nav':'CLASSES'})
-=======
 			
 @login_required(redirect_field_name='', login_url='/')
 def manualChecking(request):
 	sections = Section.objects.annotate(number_of_entries=Count('section_name')).select_related('school__short_name','section_name')
 	avatar = UserProfile.objects.get(user_id = request.user.id).avatar
 	return render(request, 'app_classes/manualChecking.html', {'avatar':avatar, 'active_nav':'CLASSES', 'sections':sections})
->>>>>>> 3a24bf539ec1dc2d9c265f68db25d37a552a8c22
