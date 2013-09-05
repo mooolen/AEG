3db28364c415496e158c3dc95394695cae7d3546
fsvaeg <fsvaeg@gmail.com>
2013-09-05 16:18:00 +0800

    classes

--- app_classes/views.py
+++ app_classes/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from app_classes.models import SectionForm, Section
+from app_classes.models import SectionForm, Section, ClassesForm
 from django.shortcuts import render
 from app_auth.models import UserProfile
 from django.db.models import Count, F
@@ -30,18 +30,17 @@ def class_teacher(request):
 @login_required(redirect_field_name='', login_url='/')
 def teacher_addNewClass(request, add_form=None):
 	avatar = UserProfile.objects.get(user_id = request.user.id).avatar
-	addClass_form = add_form or SectionForm()
+	addClass_form = add_form or ClassesForm()
 	return render(request, 'app_classes/teacher_addNewClass.html', 
 			{'addClassForm' : addClass_form, 'next_url': '/classes', 'avatar':avatar, 'active_nav':'CLASSES'})
 
 @login_required(redirect_field_name='', login_url='/')
 def submit(request):
 	if request.method == "POST":
-		form_class = SectionForm(data=request.POST)
+		form_class = ClassesForm(data=request.POST)
 		success_url = request.POST.get("next_url", "/")
 		if form_class.is_valid():
-			form_class = SectionForm(data=request.POST)
-			form_class.save()
+			form_class.save(commit=False)
 			return redirect(success_url)
 		else:
 			return teacher_addNewClass(request, form_class)
