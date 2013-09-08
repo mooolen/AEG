1b59ba3d456fa402b313455b1c0646c21a4581a6
mooolen <molenfernando@gmail.com>
2013-09-08 23:51:03 +0800

    added namespace for dashboard url

--- app_classes/urls.py
+++ app_classes/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, url
 from app_classes import views
 
 urlpatterns = patterns('',
-	url(r'^dashboard/$', views.dashboard),
+	url(r'^dashboard/$', views.dashboard, name='dashboard'),
 	url(r'^classes/$', views.class_teacher, name='classes'),
 	url(r'^classes/add/$', views.teacher_addNewClass),
 	url(r'^classes/submit/$', views.submit),
