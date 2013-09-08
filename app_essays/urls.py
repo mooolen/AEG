e333ff0485407d8a783ebc25629a640dd8f14672
Molen Fernando <molenfernando@gmail.com>
2013-09-08 23:53:17 +0800

    updated essay module

--- app_essays/urls.py
+++ app_essays/urls.py
@@ -4,4 +4,5 @@ from app_essays import views
 urlpatterns = patterns('',
 	url(r'^essays/new/$', views.new_essay, name='new'),
 	url(r'^essays/$', views.list_essay, name='list'),
+	url(r'^essays/(?P<essay_id>\d+)/$', views.essay_details, name='details'),
 )
--- app_essays/views.py
+++ app_essays/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 
-from app_auth.models import UserProfile, Teacher
+from app_auth.models import UserProfile, Student, , ClassList
 from app_essays.models import Essay, GradingSystem, EssayForm
 from app_classes.models import Subject, Classes
 
@@ -16,7 +16,7 @@ def new_essay(request):
 		if form.is_valid():
 			cd = form.cleaned_data
 			data = form.save(commit=False)
-			data.instructor = Teacher.objects.get(user_id = request.user.id) # gumagana naman
+			data.instructor = Teacher.objects.get(user_id = request.user.id)
 			data.status = 1
 			data.save()
 			return HttpResponseRedirect('/dashboard')
@@ -27,14 +27,32 @@ def new_essay(request):
 		form = EssayForm()
 		form.fields['class_subject'].queryset = Subject.objects.filter(teacher = Teacher.objects.get(user_id = request.user.id))
 	
-	return render(request, 'app_essays/new_essay.html', {'avatar':avatar, 'errors':errors, 'form': form})
+	return render(request, 'app_essays/teacher_newEssay.html', {'avatar':avatar, 'active_nav':'EXAMS', 'errors':errors, 'form': form})
 
 @login_required(redirect_field_name='', login_url='/')	
 def list_essay(request):
+	active_nav = "EXAMS"
 	avatar = UserProfile.objects.get(user_id = request.user.id).avatar
-	essays = Essay.objects.filter(instructor_id = Teacher.objects.get(user_id = request.user.id).id)
-	
-	return render(request, 'app_essays/list_essay.html', {'avatar':avatar, 'essays':essays})
-	
+	no_essay = 0
+	#IF USER IS A TEACHER
+	if len(Teacher.objects.filter(user_id = request.user.id)) > 0:
+		essays = Essay.objects.filter(instructor_id = Teacher.objects.get(user_id = request.user.id).id)
+		if (len(essays) == 0 ):
+			no_essay = 1	
+		return render(request, 'app_essays/teacher_viewEssay.html',	{'avatar':avatar, 'active_nav':'EXAMS', 'no_essay':no_essay, 'essays':essays})
+
+	#IF USER IS A STUDENT
+	elif len(Student.objects.filter(user_id = request.user.id)) > 0:
+		#essays = Essay.objects.filter(instructor_id = Teacher.objects.get(user_id = request.user.id).id)
+		essays = Essay.objects.all()
+		return render(request, 'app_essays/student_viewEssay.html', {'avatar':avatar, 'active_nav':'EXAMS', 'essays':essays})
+		
+@login_required(redirect_field_name='', login_url='/')
+def essay_details(request, essay_id):
+	active_nav = "EXAMS"
+	avatar = UserProfile.objects.get(user_id = request.user.id).avatar
+	essay = Essay.objects.get(pk=1)
+	classlist = 
+	return render(request, 'app_essays/teacher_viewEssayDetail.html', {'avatar':avatar, 'active_nav':'EXAMS', 'essay':essay})
 	
 	
