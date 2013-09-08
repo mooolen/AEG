62392d7ea36b9a7a236f274db79fbaf3b8e3aa63
mooolen <molenfernando@gmail.com>
2013-09-08 23:48:57 +0800

    modified login page

--- app_auth/forms.py
+++ app_auth/forms.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 from django import forms
 
 class LoginForm(forms.Form):
-    username = forms.CharField(widget=forms.TextInput(attrs={'type':'text', 'class': 'form-control', 'placeholder': 'Username', 'autofocus':'autofocus'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'type':'text', 'class': 'login-field', 'placeholder': 'Username', 'autofocus':'autofocus'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'login-field', 'placeholder':'Password'}))
 
 class PasswordForm(forms.Form):
 	OldPassword = forms.CharField( label='Old Password', widget=forms.PasswordInput(attrs={'type':'password', 'class': 'span3', 'placeholder': 'Required', 'name': 'newPassword'}))
