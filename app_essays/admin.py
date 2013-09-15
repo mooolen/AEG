2b691dac2762616834bebeb8f8967123acd2314f
Molen Fernando <molenfernando@gmail.com>
2013-09-16 07:33:15 +0800

    updated essay module

--- app_essays/admin.py
+++ app_essays/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from app_essays.models import GradingSystem, Grade, Essay, EssayResponse
+from app_essays.models import GradingSystem, Grade, Essay, EssayResponse, EssayComment
 
 admin.site.register(GradingSystem)
 admin.site.register(Grade)
 admin.site.register(Essay)
 admin.site.register(EssayResponse)
+admin.site.register(EssayComment)
--- app_essays/urls.py
+++ app_essays/urls.py
@@ -7,5 +7,4 @@ urlpatterns = patterns('',
 	url(r'^essays/(?P<essay_id>\d+)/$', views.essay_details, name='details'),
 	url(r'^essays/answer/(?P<essay_response_id>\d+)/$', views.answer_essay, name='answer'),
 	url(r'^essays/submissions/(?P<essay_response_id>\d+)/$', views.essay_submission, name='submission'),
-	url(r'^essays/checkManually/$', views.manualChecking),
 )
