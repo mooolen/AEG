638f18aed8e87ea7420b23d425d4f2566c7eebc1
emsia <esia.rizal@gmail.com>
2013-09-16 17:30:11 +0800

    Settings

--- AEG/settings.py
+++ AEG/settings.py
@@ -63,7 +63,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = '/home/molen/Desktop/AEG/media'
+MEDIA_ROOT = './media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
