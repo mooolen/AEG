90a9c8319a491a77e882ad93a8b7d8e5aa303f0c
Molen Fernando <molenfernando@gmail.com>
2013-09-20 13:53:31 +0800

    added proxy config in captcha, rm app_student

--- app_captcha/client.py
+++ app_captcha/client.py
@@ -110,7 +110,10 @@ def submit(recaptcha_challenge_field,
             }
         )
     
-    urllib2.install_opener(urllib2.build_opener(HTTPSHandlerV3()))
+    #SET PROXY
+    proxy_handler = urllib2.ProxyHandler({'http': 'http://proxy8.upd.edu.ph:8080/'})
+    
+    urllib2.install_opener(urllib2.build_opener(proxy_handler, HTTPSHandlerV3()))
     httpresp = urllib2.urlopen(request)
 
     return_values = httpresp.read().splitlines()
